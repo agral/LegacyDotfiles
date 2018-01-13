6b4837b374296a82e79a96c27ed3c942ef0e9ae6
aszczerbiak <szczerbiakadam@gmail.com>
2018-01-13 19:25:38 +0100

    Add a CairoHelper module
    Contains helper functions that utilize Cairo to draw amazing things
    (such as gauges, multi-colored indicators etc).

--- Conky/CairoHelper.lua
+++ Conky/CairoHelper.lua
@@ -13,6 +13,14 @@ local function AnalogGauge(x, y, val)
   cairo_move_to(cr, x, y-20)
   cairo_line_to(cr, x, y+20)
   cairo_stroke(cr)
+
+  cairo_set_line_width(cr, 10)
+  cairo_set_source_rgba(cr, 133, 153, 0, 50)
+  start_angle = -math.pi / 2
+  end_angle = 0
+  cairo_arc(cr, x, y, 60, start_angle, end_angle)
+  cairo_stroke(cr)
+
 end
 
 CairoHelper.AnalogGauge = AnalogGauge
